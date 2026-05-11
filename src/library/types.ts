9b2aa1e2322755f87e1f3b10e10317934fc87656
CrazythecatDraws <114513991+CrazythecatDraws@users.noreply.github.com>
2026-05-11 17:56:01 -0400

    revert i hate github 2

--- src/library/types.ts
+++ src/library/types.ts
@@ -12,7 +12,7 @@ type Pelt = {
   vitiligo?: string | undefined;
   eyeColour: string;
   eyeColour2?: string | undefined;
-  scars?: Array<list> | undefined;
+  scars?: Array<string> | undefined;
   tint: string;
   whitePatchesTint: string;
   accessory?: string | undefined;
@@ -35,7 +35,7 @@ type JSONData = {
   tortie_color: string | null;
   skin: string;
   tint: string;
-  scars: list | null;
+  scars: string | string[] | null;
   accessory: string | string[] | null;
 };
 
